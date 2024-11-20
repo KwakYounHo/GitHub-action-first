54f2de59167987b094c082241fa2673923211916
곽윤호 <kwakdm95@gmail.com>
2024-11-20 12:12:00 +0900

    환경변수 JSON 경로 확인용

--- .github/workflows/commit-logger.js
+++ .github/workflows/commit-logger.js
@@ -1,18 +1,24 @@
 const fs = require("fs");
 
-const commits = JSON.parse(process.env.GITHUB_EVENT_PATH).commits;
+// const commits = JSON.parse(process.env.GITHUB_EVENT_PATH).commits;
 
 const file = "log/commit/commit-log.txt";
 
-commits.forEach((commit) => {
-  const message = `*$new commit
-- commit message:
-${commit.message}
-- commit url: ${commit.url}
-- commit id: ${commit.id}
-- commit time: ${commit.timestamp}
-`;
-  fs.readFileSync(file, message, "utf8");
-});
+// try {
+//   commits.forEach((commit) => {
+//     const message = `*$new commit
+// - commit message:
+// ${commit.message}
+// - commit url: ${commit.url}
+// - commit id: ${commit.id}
+// - commit time: ${commit.timestamp}
+//   `;
+//     fs.readFileSync(file, message, "utf8");
+//   });
+// } catch (e) {
+//   console.error(e);
+// }
 
-console.log("commit saving done");
+console.log(process.env.GITHUB_EVENT_PATH);
+
+console.log("commit saving workflow is done");
