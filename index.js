0021a7c7ce8dfb758696309eabc95e3c21894095
곽윤호 <kwakdm95@gmail.com>
2024-11-20 14:40:16 +0900

    동적으로 p태그를 생성하는 js파일 생성

---
+++ index.js
@@ -0,0 +1,6 @@
+const root = document.getElementById("root");
+
+const paragraphOne = document.createElement("p");
+paragraphOne.innerHTML = "저는 Javascript에 의해 동적으로 생성되었습니다.";
+
+root.appendChild(paragraphOne);
