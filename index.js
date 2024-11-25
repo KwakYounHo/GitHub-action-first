313946bb205a18d8c13ebc204e1512cc983a6541
곽윤호 <kwakdm95@gmail.com>
2024-11-25 20:34:09 +0900

    test: CD Test

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const paragraphOne = document.createElement("p");
 paragraphOne.innerHTML = "저는 Javascript에 의해 동적으로 생성되었습니다.";
 
 root.appendChild(paragraphOne);
+
+const paragraphTwo = document.createElement("p");
+paragraphTwo.innerHTML =
+  "GitHub Actions와 Docker 그리고 AWS-S3를 이용해서 CD 파이프라인을 구축 중입니다.";
